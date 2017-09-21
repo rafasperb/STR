2a10d26c7aac4249835dd0a03cb7a7823c959ae5
Rafael Sperb <rafasperb@gmail.com>
2017-09-21 17:54:25 -0300

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int Num1;
+  int Num2;
+  cout << "Lendo o primeiro número...: ";
+  cin >> Num1;
+  cout << endl;
+  cout << "Lendo o segundo número....: ";
+  cin >> Num2;
+  cout << endl;
 
+  return 0;
+}
