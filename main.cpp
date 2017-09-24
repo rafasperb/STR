77e921652fc010ee77616c51092d52b46bc3a9ff
rafasperb <rafasperb@gmail.com>
2017-09-23 22:43:00 -0300

    a

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <stdio.h>
-//#include <vector>
-//#include <algorithm>
-//#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <list>
 using namespace std;
 
 //(SCHED_FIFO correspondendo a 1 e SCHED_RR, a 2)
@@ -16,13 +17,23 @@ int main()
   int ci; //tempo de computação da tarefa (Ci)
   int si; //tempo de chegada da tarefa (Si)
   int pi; //prioridade (pi)
+  int aux;
 //##############################################################
+std::list<int> tarefas;   // cria uma lista de pronto
+std::list<int>::iterator t;  // cria um iterador
+std::list<int> pronto;   // cria uma lista de pronto
+std::list<int>::iterator p;  // cria um iterador
 
   cin >> n;
   cout << endl;
+for(aux=0;aux < n;aux++)
+{
   cin >> ci;
   cin >> si;
   cin >> pi;
+  cout << endl;
+}
+
   cout << endl;
   cout << endl;
   cout << "N# tarefas:" << n <<"tempo computação: " <<ci <<"tempo chegada:" << si <<"prioridade:" << pi;
