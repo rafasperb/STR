a4e88169c090739628d3d42aa6b9dee39cd84da7
Rafael Sperb <rafasperb@gmail.com>
2017-10-03 12:05:50 -0300

    1

--- main.cpp
+++ main.cpp
@@ -4,20 +4,92 @@
 #include <algorithm>
 #include <string.h>
 #include <list>
+#include "main.h"
 using namespace std;
 
 //(SCHED_FIFO correspondendo a 1 e SCHED_RR, a 2)
 //A descrição de cada tarefa é composta por três valores que correspondem
 //respectivamente a: tempo de computação da tarefa (Ci), tempo de chegada da tarefa (Si) e prioridade
 //(pi) da tarefa
+
+void Tarefa::inicializa(int id, int ci, int si, int pi, int po)
+{
+  id = id;
+  ci = ci;
+  si = si;
+  pi = pi;
+  po = po;
+}
+
+int Tarefa::get_id()
+{
+  return id;
+}
+
+int Tarefa::get_tempocomp()
+{
+  return ci;
+}
+
+int Tarefa::get_chegada()
+{
+  return si;
+}
+
+int Tarefa::get_prioridade()
+{
+  return pi;
+}
+
+int Tarefa::get_politica()
+{
+  return po;
+}
+
+void Tarefa::set_id(int id)
+{
+  id = id;
+}
+
+void Tarefa::set_tempocomp(int ci)
+{
+  ci = ci;
+}
+
+void Tarefa::set_chegada(int si)
+{
+  si = si;
+}
+
+void Tarefa::set_prioridade(int pi)
+{
+  pi = pi;
+}
+
+void Tarefa::set_politica(int po)
+{
+  po = po;
+}
+
+void Tarefa::imprime()
+{
+ cout << "Id: " << id << endl;
+ cout << "Tempo Computação: " << ci << endl;
+ cout << "Tempo de chegada: " << si << endl;
+ cout << "Prioridade: " << pi << endl;
+ cout << "Politica: " << po << endl;
+}
+
 int main()
 {
 //##############################################################
-  int n; //numero de tarefas.
-  int ci; //tempo de computação da tarefa (Ci)
-  int si; //tempo de chegada da tarefa (Si)
-  int pi; //prioridade (pi)
-  int aux;
+  int n=0; //numero de tarefas.
+  int id=0; // id da tarefa
+  int ci=0; //tempo de computação da tarefa (Ci)
+  int si=0; //tempo de chegada da tarefa (Si)
+  int pi=0; //prioridade (pi)
+  int po=0; // politica (po)
+  int aux=0,i=0;
 //##############################################################
 std::list<int> tarefas;   // cria uma lista de pronto
 std::list<int>::iterator t;  // cria um iterador
@@ -26,18 +98,34 @@ std::list<int>::iterator p;  // cria um iterador
 
   cin >> n;
   cout << endl;
+
+Tarefa teste[n];
+
+for(aux=0;aux < n;aux++)
+{
+  teste[aux].inicializa(id,ci,si,pi,po);
+  teste[aux].imprime();
+}
+
 for(aux=0;aux < n;aux++)
 {
   cin >> ci;
+  teste[aux].set_tempocomp(ci);
   cin >> si;
+  teste[aux].set_chegada(si);
   cin >> pi;
-  cout << endl;
-}
+  teste[aux].set_prioridade(pi);
+  cin >> po;
+  teste[aux].set_politica(po);
+  teste[aux].set_id(i);
+  i++;
 
   cout << endl;
-  cout << endl;
-  cout << "N# tarefas:" << n <<"tempo computação: " <<ci <<"tempo chegada:" << si <<"prioridade:" << pi;
-  cout << endl;
+}
+for(aux=0;aux < n;aux++)
+{
+    teste[aux].imprime();
+}
 
   return 0;
 }
--- main.h
+++ main.h
@@ -4,15 +4,24 @@ using namespace std;
 class Tarefa
 {
 
-  char id;
+  int id;
   int ci; //tempo de computação da tarefa (Ci)
   int si; //tempo de chegada da tarefa (Si)
   int pi; //prioridade (pi)
+  int po; //politica
 
 public:
 
-  void inicializa(string n, float s);
-  void deposita(float valor);
-  void consulta();
-  int saque(float valor);
+  void inicializa(int id, int ci, int si, int pi, int po);
+  int get_id();
+  int get_tempocomp();
+  int get_chegada();
+  int get_prioridade();
+  int get_politica();
+  void set_id(int id);
+  void set_tempocomp(int ci);
+  void set_chegada(int si);
+  void set_prioridade(int pi);
+  void set_politica(int po);
+  void imprime();
 };
